dea36513a8707662fa71cb051d41890d656faf23
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-08-02 17:43:09 +0200

    added some style

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,7 +1,17 @@
 'use strict'
-
 const express = require('express');
-
 const router = express.Router();
-
 module.exports = router;
+
+const User = require('./../models/user');
+const Chef = require('./../models/chef');
+const Booking = require('./../models/booking');
+const Recipe = require('./../models/recipe');
+
+//ROUTER SIGNUP CHEF
+router.get('/bookings', (req, res, next) => {
+    res.render('myBookings/bookings', {
+        errorMessage: ''
+    });
+    return
+});
