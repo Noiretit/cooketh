62ae6a5efb70ac2cbd316e12ad593d4d3af47bb1
Noiret <sergio.ko@icloud.com>
2020-08-02 13:01:04 +0200

    Profile chef and user created: hbs and routes. Rearranged buttons. Users only see user buttons and chef only see chef buttons

--- routes/chef.js
+++ routes/chef.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
+const Chef = require('../models/chef')
 const Recipe = require('../models/recipe');
 const Booking = require('../models/booking')
 
@@ -14,6 +15,19 @@ router.use((req, res, next) => {
         return;
     }
     res.redirect('/login');
+});
+
+router.get('/profile-chef', (req, res, next) => {
+    Chef.findById(req.session.currentUser._id)
+        .then(thisChefDB => {
+            res.render('profiles/chef-profile.hbs', {
+                thisChef: thisChefDB
+            })
+        })
+        .catch((err) => {
+            console.log('Error while displaying chef profile', err)
+            next(err)
+        })
 });
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,34 @@
 'use strict'
-
 const express = require('express');
-
 const router = express.Router();
+
+const User = require('../models/user');
+const Chef = require('../models/chef')
+const Recipe = require('../models/recipe');
+const Booking = require('../models/booking')
+
+// MIDDLEWARE =>
+//If user is connected, go to next routes
+router.use((req, res, next) => {
+    if (req.session.currentUser) {
+        next();
+        return;
+    }
+    res.redirect('/login');
+});
+
+router.get('/profile-user', (req, res, next) => {
+    User.findById(req.session.currentUser._id)
+        .then(thisUserDB => {
+            res.render('profiles/user-profile.hbs', {
+                thisUser: thisUserDB
+            })
+        })
+        .catch((err) => {
+            console.log('Error while displaying user profile', err)
+            next(err)
+        })
+});
+
 
 module.exports = router;
